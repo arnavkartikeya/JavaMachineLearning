38ededff6d9bad223d930bcaadbc30ada0c1764d
arnavkartikeya <arnavkartikeya@gmail.com>
2021-12-07 15:07:55 -0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,15 @@ import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-
+        //reading data
         Matrix data = Matrix.readTxt(new File ("src/test.txt"),20, 2 );
         Matrix x = data.getCol(0);
         Matrix y = data.getCol(1);
-
+        //fitting model
         LinearRegressionModel l = new LinearRegressionModel(x, y);
-
         Metrics test = new MeanSquaredLoss(100, null, y );
-
-
         Matrix weights =  l.fit(1000,0.01 , test);
-
+        //outputs
         double[][] testValues = {{7}};
         Matrix values = new Matrix(testValues);
         Matrix answers = l.predict(values);
